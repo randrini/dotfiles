084038d18cbfda2552777bcaef88119c362d2b41
randrini <richard.andrinirina@gmail.com>
2017-05-06 01:11:12 +0200

    Getting Closer

--- init.sh
+++ init.sh
@@ -3,17 +3,7 @@
 #Essential packages
 #sudo pacman -Syyu
   sudo pacman --needed --noconfirm -S zsh tmux the_silver_searcher terminator neovim python-pip python-setuptools ttf-hack 
-  sudo pacman -Syu --force python2-neovim python-neovim
-
-#Fonts
-#  yaourt --needed --noconfirm -S fira-code-git
-
-# Vim-plug for Neovim
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Install Neovim for python
-# sudo pip3 install neovim
+  #sudo pacman -Syu --force python2-neovim python-neovim
 
 #Set ZSH as default shell
   chsh -s $(which zsh)
@@ -24,10 +14,16 @@
   rm -rf ~/.config/rofi
   mkdir -p ~/.config ~/.config/nvim  # ~/.config/rofi/
 
+# Vim-plug for Neovim
+ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
 # Sym link dots
-  ln -s ~/dotfiles/zshrc ~/.zshrc
-  ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-  ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
-  ln -s ~/dotfiles/vimscheme/base16-harmonic16-dark.16.theme ~/.local/share/xfce4/terminal/colorschemes/harmonic.theme
-#  ln -s ~/dotfiles/rofic ~/.config/rofi/config
+  ln -s ~/dotfiles/zsh_conf ~/.zshrc
+  ln -s ~/dotfiles/tmux_conf ~/.tmux.conf
+  ln -s ~/dotfiles/nvim_conf ~/.config/nvim/init.vim
+  #ln -s ~/dotfiles/vimscheme/base16-harmonic16-dark.16.theme ~/.local/share/xfce4/terminal/colorschemes/harmonic.theme
+# ln -s ~/dotfiles/rofic ~/.config/rofi/config
+
 #----------------------- END OF Preconfig ----------------------------
